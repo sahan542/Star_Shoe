fe6372f23a4ea11d992f022260a2306e7223522e
sahan542 <sahanrashmika542@gmail.com>
2023-08-20 13:08:01 +0530

    Server Setup

---
+++ server.js
@@ -0,0 +1,25 @@
+import express from 'express';
+import colors  from 'colors';
+import dotenv from 'dotenv';
+
+//configure env
+dotenv.config();
+
+
+//rest object
+const app= express();
+
+//rest api
+app.get('/' , (req,res) => {
+    res.send("<h1>Welcome to E-Commerce Apppp</h1>"
+
+    );
+});
+
+//PORT
+const PORT = process.env.PORT || 8080;
+
+//run listen
+app.listen(PORT, () => {
+    console.log('Server Running On ${process.env.DEV_MODE} mode on port ${PORT}' .bgCyan.white);
+});

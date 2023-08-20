b3a799deacae527d67f3bcc872f0f413569f47e7
sahan542 <sahanrashmika542@gmail.com>
2023-08-20 19:08:09 +0530

    Connected to DB

--- server.js
+++ server.js
@@ -1,17 +1,28 @@
 import express from 'express';
 import colors  from 'colors';
 import dotenv from 'dotenv';
+import morgan from 'morgan';
+import connectDB from './config/db.js';
+
+//const dbURI = 'mongodb+srv://star_shoe:%23A991572929v%26@cluster1.q7svok4.mongodb.net/';
 
 //configure env
 dotenv.config();
+
+//databse config
+connectDB();
 
 
 //rest object
 const app= express();
 
+//middlewares
+app.use(express.json())
+app.use(morgan('dev'))
+
 //rest api
 app.get('/' , (req,res) => {
-    res.send("<h1>Welcome to E-Commerce Apppp</h1>"
+    res.send("<h1>Welcome to E-Commerce App</h1>"
 
     );
 });
